a98c9bd48764f118b4abdbeaa4b3ce32810c7d74
gauravhisar <mr.gaurav.hisar2001@gmail.com>
2021-06-24 23:25:05 +0530

    Added dues in plot view

--- src/components/Deal/Deal.jsx
+++ src/components/Deal/Deal.jsx
@@ -10,11 +10,11 @@ export default function Deal({ title, base_url, project_id, plot_details, setPlo
 
 	// const [plot_details,setPlotDetails] = useState({})
 
-	const deal_endpoint      = base_url + `projects/${project_id}/plots/${plot_details.deal.id}/`
+	const deal_endpoint = base_url + `projects/${project_id}/plots/${plot_details.deal.id}/`
 	const customers_endpoint = base_url + `customers/`
-	const customer_endpoint  = `${plot_details.deal.customer.id}/`
-	const dealers_endpoint   = base_url + `dealers/`
-	const dealer_endpoint    = `${plot_details.deal.dealer.id}/`
+	const customer_endpoint = `${plot_details.deal.customer.id}/`
+	const dealers_endpoint = base_url + `dealers/`
+	const dealer_endpoint = `${plot_details.deal.dealer.id}/`
 
 	const [editing_view, setEditingView] = useState(false)
 
@@ -22,12 +22,12 @@ export default function Deal({ title, base_url, project_id, plot_details, setPlo
 		alert("Deletion Not Compatible Yet")
 	}
 
-	const verticallyCenter = {display:'flex',alignItems: 'center'}
+	const verticallyCenter = { display: 'flex', alignItems: 'center' }
 
-	if (editing_view){
-		return <AddDeal title = "Deals" base_url={base_url} project_id = {project_id} plot_details={plot_details} setPlotDetails={setPlotDetails} index={plot_details.id} setEditingView={setEditingView}  />
+	if (editing_view) {
+		return <AddDeal title="Deals" base_url={base_url} project_id={project_id} plot_details={plot_details} setPlotDetails={setPlotDetails} index={plot_details.id} setEditingView={setEditingView} />
 	}
-	else{
+	else {
 		return (
 			<>
 				<div className="card col-lg-7 mx-3">
@@ -36,33 +36,24 @@ export default function Deal({ title, base_url, project_id, plot_details, setPlo
 						<div className="card-text">
 							<form>
 								<div className="row mb-3">
-									{/* <label htmlFor="customer-no" className="col-sm-2 col-form-label" style= {verticallyCenter}>Customer PNo:</label> */}
-									<div className="col-sm-4" style= {verticallyCenter}>
-										<TextField label="Customer Name" InputProps = {{readOnly: true }} margin = "dense" size = "small" color = "primary" variant="standard" value = {plot_details.deal.customer.name} />
-										{/* <input type="text" className="form-control" id="customer-no" value = {plot_details.deal.customer.contact_no} disabled={true} /> */}
+									<div className="col-sm-4" style={verticallyCenter}>
+										<TextField label="Customer Name" InputProps={{ readOnly: true }} margin="dense" size="small" color="primary" variant="standard" value={plot_details.deal.customer.name} />
 									</div>
-									<div className="col-sm-4" style= {verticallyCenter}>
-										<TextField label="Customer Contact" InputProps = {{readOnly: true }} margin = "dense" size = "small" color = "primary" variant="standard" value = {plot_details.deal.customer.contact_no} />
-										{/* <input type="text" className="form-control" id="customer-no" value = {plot_details.deal.customer.contact_no} disabled={true} /> */}
+									<div className="col-sm-4" style={verticallyCenter}>
+										<TextField label="Customer Contact" InputProps={{ readOnly: true }} margin="dense" size="small" color="primary" variant="standard" value={plot_details.deal.customer.contact_no} />
 									</div>
 								</div>
 								<div className="row mb-3">
-									{/* <label htmlFor="dealer-no" className="col-sm-2 col-form-label" style= {verticallyCenter}>Dealer PNo:</label> */}
-									<div className="col-sm-4" style= {verticallyCenter}>
-										<TextField label="Dealer Name" InputProps = {{readOnly: true }} margin = "dense" size = "small" color = "primary" variant="standard" value = {plot_details.deal.dealer.name} />
-										{/* <input type="text" className="form-control" id="dealer-no" value={plot_details.deal.dealer.contact_no || ""} disabled={true} /> */}
+									<div className="col-sm-4" style={verticallyCenter}>
+										<TextField label="Dealer Name" InputProps={{ readOnly: true }} margin="dense" size="small" color="primary" variant="standard" value={plot_details.deal.dealer.name} />
 									</div>
-									{/* <label htmlFor="dealer-name" className="col-sm-2 col-form-label" style= {verticallyCenter}>Dealer Name:</label> */}
-									<div className="col-sm-4" style= {verticallyCenter}>
-										<TextField label="Dealer Contact" InputProps = {{readOnly: true }} margin = "dense" size = "small" color = "primary" variant="standard" value = {plot_details.deal.dealer.contact_no} />
-										{/* <input type="text" className="form-control" id="dealer-name" value={plot_details.deal.dealer.name || ""} disabled={true} /> */}
+									<div className="col-sm-4" style={verticallyCenter}>
+										<TextField label="Dealer Contact" InputProps={{ readOnly: true }} margin="dense" size="small" color="primary" variant="standard" value={plot_details.deal.dealer.contact_no} />
 									</div>
 								</div>
 								<div className="row mb-3">
-									{/* <label htmlFor="customer-no" className="col-sm-2 col-form-label" style= {verticallyCenter}  >Balance:</label> */}
-									<div className="col-sm-4" style= {verticallyCenter}>
-										<TextField label="Balance" InputProps = {{readOnly: true }} margin = "dense" size = "small" color = "primary" variant="standard" value = {plot_details.deal.balance} />
-										{/* <input type="text" className="form-control" id="customer-no" value={plot_details.deal.balance || ""}  disabled={true} /> */}
+									<div className="col-sm-4" style={verticallyCenter}>
+										<TextField label="Balance" InputProps={{ readOnly: true }} margin="dense" size="small" color="primary" variant="standard" value={plot_details.deal.balance} />
 									</div>
 								</div>
 								<div style={{ textAlign: 'right' }}>
@@ -79,8 +70,3 @@ export default function Deal({ title, base_url, project_id, plot_details, setPlo
 
 
 }
-
-								// <div style={{ textAlign: 'right' }}>
-								// 	<button onClick={editItem} style={{ margin: '5px 5px' }} type='submit' className="btn btn-sm btn-primary">&nbsp;Save&nbsp;&nbsp;</button>
-								// 	<button onClick={(e) => { e.preventDefault(); setEditingView(false); }} style={{ margin: '5px 5px' }} className="btn btn-sm btn-danger">Cancel</button>
-								// </div>
